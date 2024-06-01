ed6becd85bf52abf09c207c4e584d8451e1c9baa
Richard Hunt <116373680+katawart@users.noreply.github.com>
2024-06-01 09:39:12 +0100

    first commit

---
+++ notes/app.js
@@ -0,0 +1,54 @@
+var createError = require('http-errors');
+const express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
+const notes = require('./routes/notes');
+
+const hbs = require('hbs');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.use('/assets/vendor/bootstrap', express.static(
+  path.join(__dirname, 'node_modules', 'bootstrap', 'dist')));
+app.use('/assets/vendor/jquery', express.static(
+  path.join(__dirname, 'node_modules', 'jquery')));
+app.use('/assets/vendor/popper.js', express.static(
+  path.join(__dirname, 'node_modules', 'popper.js', 'dist')));
+
+app.set('view engine', 'hbs');
+app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, 'partials'));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/assets/vendor/feather-icons', express.static(
+  path.join(__dirname, 'node_modules', 'feather-icons', 'dist')));
+app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+app.use('/notes', notes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
